0bd39f786ca69c234046e7cc79cc0db91aff5811
Agata-Carvalho <agataritacarvalho.youdance@gmail.com>
2021-04-19 11:29:14 +0100

    Forecast Update (Functioning)

--- index.js
+++ index.js
@@ -1,42 +1,56 @@
 function formatdate() {
+  
+  let now = new Date();
+  let day = now.getDay();
+  let month = now.getMonth();
+  let date = now.getDate();
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday"
+  ];
+  day = days[now.getDay()];
+  let months = [
+    "January",
+    "February",
+    "March",
+    "April",
+    "May",
+    "June",
+    "July",
+    "August",
+    "September",
+    "October",
+    "November",
+    "December"
+  ];
+  month = months[now.getMonth()];
+  let hour = now.getHours();
+  let minutes = now.getMinutes();
+  
   let today = document.querySelector("#date");
   today.innerHTML = `${day}, ${month} ${date}, ${hour}:${minutes}`;
 }
 
-let now = new Date();
-let day = now.getDay();
-let month = now.getMonth();
-let date = now.getDate();
-let days = [
-  "Sunday",
-  "Monday",
-  "Tuesday",
-  "Wednesday",
-  "Thursday",
-  "Friday",
-  "Saturday"
-];
-day = days[now.getDay()];
-let months = [
-  "January",
-  "February",
-  "March",
-  "April",
-  "May",
-  "June",
-  "July",
-  "August",
-  "September",
-  "October",
-  "November",
-  "December"
-];
-month = months[now.getMonth()];
-let hour = now.getHours();
-let minutes = now.getMinutes();
-
 formatdate();
 
+function formatDay(timestamp) {
+  let date = new Date(timestamp);
+  let days = [
+    "Sun",
+    "Mon",
+    "Tue",
+    "Wed",
+    "Thu",
+    "Fri",
+    "Sat",
+  ];
+  return days[date.getDay()];
+}
 
 // Forecast Weather //
 
@@ -45,12 +59,13 @@ let forecast = response.data.daily;
 let forecastElement = document.querySelector("#forecast");
 
 let forecastHTML = `<div class="row">`;
+
 forecast.forEach(function(forecastDay, index) {
-  if (index < 6) {
+  if (index < 7) {
     forecastHTML= forecastHTML +
     `
-    <div class="col-2">
-      <div class="weather-forecast-date">${formatDay(forecastDay.dt)}</div>
+    <div class="col">
+      <div class="weather-forecast-date">${formatDay(forecastDay.dt * 1000)}</div>
       <img src="http://openweathermap.org/img/wn/${forecastDay.weather[0].icon}@2x.png"
        alt=""
        width="45"></img>
@@ -66,6 +81,7 @@ forecast.forEach(function(forecastDay, index) {
   `;
     }
   });
+
   forecastHTML = forecastHTML + `</div>`;
   forecastElement.innerHTML = forecastHTML;
 }
@@ -74,7 +90,7 @@ forecast.forEach(function(forecastDay, index) {
 function getForecast(coordinates) {
   let apiKey = "e19aef4bcf8f9de7fd9dad6710436c3e";
   let apiURL = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`;
-  axios.get(apiUrl).then(displayForecast);
+  axios.get(apiURL).then(displayForecast);
 }
 
 // Temperature //
